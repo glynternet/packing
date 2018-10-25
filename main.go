c76c35679e77c542c1a5cd9be6f54b82416c2202
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-10-25 21:43:44 +0100

    Print Individual Items before sublists
    Prior to this commit, the items that were unique to the parent list
    would be printed within the rest of the groups that are loaded from the
    sublists.
    
    This change ensures that the Individual Items group is always printed at
    the topby not adding it to the map formed of the sublists.

--- main.go
+++ main.go
@@ -39,18 +39,6 @@ func run(path string, listsDir string, logger *log.Logger, w io.Writer) error {
 	}
 
 	groups := make(map[string]list.Group)
-	if len(cs.Items) > 0 {
-		// BUG(glyn): By adding this to the map here, it means that if there is a
-		// sublist later in the application called "Individual Items", it will not
-		// be added to the map. If we move adding the "Individual Items" to later
-		// in the flow, we can check if it exists already, then name it something
-		// else that has not yet been taken.
-		name := "Individual Items"
-		groups[name] = list.Group{
-			Name:  name,
-			Items: cs.Items,
-		}
-	}
 
 	loader := file.Getter{
 		DirPath: listsDir,
@@ -60,6 +48,14 @@ func run(path string, listsDir string, logger *log.Logger, w io.Writer) error {
 	err = load.GroupsRecursively(cs.SublistKeys, logger, loader, groups)
 	if err != nil {
 		return errors.Wrap(err, "loading groups recursively")
+	}
+
+	if len(cs.Items) > 0 {
+		write.Group(w, list.Group{
+			Name:  "Individual Items",
+			Items: cs.Items,
+		})
+		write.GroupBreak(w)
 	}
 
 	for _, g := range groups {
