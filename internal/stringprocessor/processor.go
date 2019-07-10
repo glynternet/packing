f3cd0a74791777318dbf49d9bb5310d705a6bbf3
Glyn Owen Hanmer <glynhanmer@gmail.com>
2019-07-10 09:15:18 +0100

    #(no branch, rebasing master): #(no branch, rebasing master): 📝 adding comments and function docs

--- internal/stringprocessor/processor.go
+++ internal/stringprocessor/processor.go
@@ -8,22 +8,28 @@ import (
 	"github.com/glynternet/packing/pkg/parse"
 )
 
+// Processor should return an error if the string cannot be processed as the given type
 type Processor func(string) error
 
+// Group is a group of processors
 type Group []Processor
 
+// Process applies each Processor in the Group in order until either:
+// - no error is given, in which case nil is returned.
+// - all Processors have been applied, in which case the last is returned
 func (g Group) Process(s string) error {
 	for _, processFn := range g {
 		err := processFn(s)
 		if err == nil {
 			return err
 		}
-		//TODO: use multi error here?
+		//TODO(glynternet): use multi error here?
 	}
 	return fmt.Errorf("unable to Process string:%q", s)
 }
 
 func ItemNamesProcessor(names *[]string) Processor {
+	// TODO(glynternet): does this need to be improved to ignore groups and other cases?
 	return func(s string) error {
 		name, err := parse.NotEmpty(s)
 		if err == nil {
