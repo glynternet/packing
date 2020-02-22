69b153f0f263a8b7c542ed68936880fd6edc5f84
Glyn Owen Hanmer <glynhanmer@gmail.com>
2020-02-22 19:02:51 +0000

    ✨ Print groups that are comprised of only groups

--- pkg/graph/group.go
+++ pkg/graph/group.go
@@ -8,3 +8,11 @@ type Group struct {
 	api.Group
 	ImportedBy []string
 }
+
+func (g Group) HasContents() bool {
+	if len(g.Contents.Items) != 0 {
+		return true
+	}
+
+	return len(g.Contents.GroupKeys) != 0
+}
