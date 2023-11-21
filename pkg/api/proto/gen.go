07353e76b6f85eb308498bf58c9d6e24f9e86ccb
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2023-11-20 20:26:38 -0700

    💡 tools to install

--- pkg/api/proto/gen.go
+++ pkg/api/proto/gen.go
@@ -1,3 +1,5 @@
 package proto
 
+// apt install protobuf-compiler protoc-gen-go
+// go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 //go:generate bash -c "mkdir -p ../build && protoc --go_out=../build --go_opt=paths=source_relative --go-grpc_out=../build --go-grpc_opt=paths=source_relative ./*.proto"
