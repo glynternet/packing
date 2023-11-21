3cbd6b17d675c6000b7461b0d4aa3b357a8df4a4
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2023-11-20 20:22:42 -0700

    🐛 mkdir -p

--- pkg/api/proto/gen.go
+++ pkg/api/proto/gen.go
@@ -1,3 +1,3 @@
 package proto
 
-//go:generate bash -c "mkdir ../build && protoc --go_out=../build --go_opt=paths=source_relative --go-grpc_out=../build --go-grpc_opt=paths=source_relative ./*.proto"
+//go:generate bash -c "mkdir -p ../build && protoc --go_out=../build --go_opt=paths=source_relative --go-grpc_out=../build --go-grpc_opt=paths=source_relative ./*.proto"
